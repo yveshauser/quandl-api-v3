3fa3973df0959458fa6f6fdbfc658a5412e991cd
Yves Hauser <yves.hauser@gmail.com>
2022-10-28 10:03:49 +0200

    nix flakes

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Quandl API V3";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskellPackages;
+        packageName = "quandl-api-v3";
+      in {
+        packages.${packageName} =
+          haskellPackages.callCabal2nix packageName self rec {
+          };
+        defaultPackage = self.packages.${system}.${packageName};
+        devShell =
+          let libs = [
+            pkgs.zlib
+          ];
+          in
+            pkgs.mkShell {
+              buildInputs = with pkgs; [
+                haskellPackages.haskell-language-server
+                cabal-install
+              ];
+              inputsFrom = builtins.attrValues self.packages.${system};
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+          };
+      });
+}
